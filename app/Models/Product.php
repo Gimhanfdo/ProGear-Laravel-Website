24a65bfed6c9e1c0239fe5009ce8760c1abe0896
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-10-02 22:40:02 +0530

    updated Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,16 @@ class Product extends Model
 {
     //Specifies which fields to enter into DB
     protected $fillable = ['name', 'brand', 'category', 'price', 'productimage', 'description', 'discountpercentage', 'quantityavailable'];
+
+    // Scope for discounted products
+    public function scopeDiscounted($query)
+    {
+        return $query->where('discountpercentage', '>', 0);
+    }
+
+    // Scope for filtering by category
+    public function scopeByCategory($query, $category)
+    {
+        return $query->whereRaw('LOWER(category) = ?', [strtolower($category)]);
+    }
 }
